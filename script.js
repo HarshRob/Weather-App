6cd529554cc6791e8750a25c591dd039d7f54456
Robbie Harshman <robbie.harshman@gmail.com>
2022-01-29 06:45:59 -0800

    Setting Up Fetch Command

---
+++ script.js
@@ -0,0 +1,25 @@
+let weather = {
+    "apiKey": "f04d78e48cbdbcc5aff065cb5fd86d80",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=imperial&appid="
+            + this.apiKey
+            ).then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".temp").innerText = Math.round(temp) + "° F";
+        document.querySelector(".weather-type").innerText = description.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + Math.round(speed) + " mph";
+    }
+};
