85cd4e3aeea3bb4e816f0fee7dd8000883d009d5
Robbie Harshman <robbie.harshman@gmail.com>
2022-02-07 13:17:06 -0800

    Added Changing Backgrounds

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const searchElement = document.querySelector('[data-city-search]')
-const searchBox = new google.maps.places.SearchBox(searchElement)
-searchBox.addListener('places_changed', () => {
-    const place = searchBox.getPlaces()[0]
-    if (place == null) return
-    const name = place.name
+const options = {types: ['(cities)']}
+const autoComplete = new google.maps.places.Autocomplete(searchElement, options)
+
+function search() {
+    const name = document.querySelector(".search-bar").value
     const units = document.getElementById("units").checked ? "metric" : "imperial"
-    
+    if(name == "") return
     fetch('/weather', {
         method: 'POST' ,
         headers: {
@@ -16,31 +16,37 @@ searchBox.addListener('places_changed', () => {
             name: name,
             units: units
         })
-    }).then(res => res.json()).then(data => {
-        setWeatherData(data, place.formatted_address)
+    }).then(res => res.json()).then((data) => {
+        setWeatherData(data)
     })
-})
+}
 
 function setWeatherData(data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name,icon,description,temp,humidity,speed)
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     document.querySelector(".temp").innerText = document.getElementById("units").checked ? Math.round(temp) + "°C" : Math.round(temp) + "°F";
     document.querySelector(".weather-type").innerText = description
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = document.getElementById("units").checked ? "Wind Speed: " + Math.round(speed) + " m/s" : "Wind Speed: " + Math.round(speed) + " mph";
-}
-
-function search() {
-    setWeatherData(document.querySelector(".search-bar").value, place.formatted_address);
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + name + "')"
 }
 
 document.querySelector(".search button").addEventListener("click", function() {
-    search();
+    search()
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e) {
+    if(e.key == "Enter") {
+        search()
+    }
+})
+
+document.getElementById("units").addEventListener("click", function() {
+    search()
 })
 
 
